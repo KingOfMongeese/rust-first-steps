0ccd028a0ae284e2e2ab2021ce923e9418eb51f9
KingOfMongeese <nwohletz@gmail.com>
2025-03-12 22:37:25 -0600

    fix(word-count): normalize whitespace

--- word-count/src/main.rs
+++ word-count/src/main.rs
@@ -10,16 +10,34 @@ struct Cli {
     delimiter: String,
 }
 
+fn normalize_white_space(string: &mut String) {
+    *string = string.trim().to_string();
+
+    let mut prev = 'a';
+    let mut temp_str = String::new();
+    for c in string.chars() {
+        if c == ' ' && prev == ' ' {
+            prev = c;
+            continue;
+        }
+        temp_str += &c.to_string();
+    }
+
+    *string = temp_str;
+}
+
 fn main() {
     let cli = Cli::parse();
 
-    if let Some(string) = cli.string {
+    if let Some(mut string) = cli.string {
+        normalize_white_space(&mut string);
         let words: Vec<String> = string
             .split(&cli.delimiter)
             .map(|word| String::from(word))
             .collect();
         let count = words.len();
         println!("{count}");
+        println!("{:?}", words);
     } else {
         eprintln!("No data");
         std::process::exit(1)
